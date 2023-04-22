92b835200a219e67923ff4d44eec5219bc8202ad
Hans Kirchner <hans-kirchner@gmx.de>
2023-04-22 11:09:39 +0200

    Add IonDatetime

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,12 +1,37 @@
-import './ExploreContainer.css';
+import {
+  IonButton,
+  IonContent,
+  IonDatetime,
+  IonDatetimeButton,
+  IonModal,
+} from "@ionic/react";
+import "./ExploreContainer.css";
+import { useState } from "react";
 
-interface ContainerProps { }
+interface ContainerProps {}
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
+  const [isOpen, setOpen] = useState(false);
+
   return (
     <div className="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <IonDatetimeButton datetime="datetime">
+        <div slot="date-target">Bla</div>
+      </IonDatetimeButton>
+
+      <IonModal keepContentsMounted={true}>
+        <IonDatetime id="datetime"></IonDatetime>
+      </IonModal>
+
+      <IonButton onClick={() => setOpen(true)}>Open</IonButton>
+
+      <IonModal
+        isOpen={isOpen}
+        onDidDismiss={() => setOpen(false)}
+        className="ion-datetime-button-overlay"
+      >
+        <IonDatetime presentation="date"></IonDatetime>
+      </IonModal>
     </div>
   );
 };
